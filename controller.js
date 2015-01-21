25164945a82be1bc088dae34ec1dedd2e44f49f7
Ty Frith <tyfrith@gmail.com>
2015-01-21 12:17:34 -0700

    complete mini-userProfiles2

--- controller.js
+++ controller.js
@@ -1,14 +1,14 @@
 var app = angular.module('userProfiles');
 
-app.controller('MainController', function($scope, mainService) {
-  $scope.getUsers = function() {
-    mainService.getUsers().then(function(data) {
-      $scope.users = data.data.data;
-      console.log($scope.users)
+app.controller('MainController', function($scope, mainService) { //defining mainController, giving it parameters of $scopr and our service
+  $scope.getUsers = function() {         //getUsers is equal to function
+    mainService.getUsers().then(function(response) {   //.then initiates the promise and once the promise object is received, the function runs.
+      $scope.users = response.data.data;      //users is equal the response from $http request 
+      // console.log($scope.users)
     });
-    console.log($scope.users);
+    // console.log($scope.users);
   }
 
-  $scope.getUsers();
-
+  $scope.getUsers();  //All we are doing here is calling the mainService.getUsers function, and saying that once we finish that function, THEN define $scope.users.
+  console.log($scope.users);
 });
--- service.js
+++ service.js
@@ -1,12 +1,13 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function($http) {    //http injected into the service
 
-
-  this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqr.es/api/users?page=3'
+this.getUsers = function() {
+    return $http({ 				//using built in http service
+        method: 'GET',                //"get" data from the server
+        url: 'http://reqr.es/api/users?page=3'  //url where we get data from (endpoint) Supplied by backend
     })
   }
 });
+
+
